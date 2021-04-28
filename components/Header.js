067af627ef01de0d3f29b77df1626ed371317933
Dimas W <daimessdn@gmail.com>
2021-04-29 00:57:12 +0700

    resoved fonts and subtitle section

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,55 @@
 import Link from "next/link";
+import Image from "next/image";
+
+import styles from "../styles/Header.module.css";
 
 const Header = () => {
   return (
     <header>
-      <h1>Welcome to chatroom</h1>
+      <h1>Welcome to chatroom!</h1>
+
+      <div className={styles.subtitleSection}>
+        <div className={styles.chatSubtitleLeft}>
+          <img
+            className={styles.profileChatDemo}
+            src="/img/profiledemo2.jpg"
+            alt="profile demo 1"
+           />
+          <p>Chatroom lets you communicate with people.</p>
+        </div>
+
+        <div className={styles.chatSubtitleRight}>
+          <img
+            className={styles.profileChatDemo}
+            src="/img/profiledemo1.jpg"
+            alt="profile demo 2"
+           />
+          <p>And also a wonderful place to make a discussion.</p>
+        </div>
+
+        <div className={styles.chatSubtitleLeft}>
+          <img
+            className={styles.profileChatDemo}
+            src="/img/profiledemo2.jpg"
+            alt="profile demo 1"
+           />
+          <p>Absolutely, Harold! Why don't you also join with us?</p>
+        </div>
+
+        <div className={styles.chatSubtitleRight}>
+          <img
+            className={styles.profileChatDemo}
+            src="/img/profiledemo1.jpg"
+            alt="profile demo 2"
+           />
+          <p>You're right, Anna! Let's join us by clicking the link below.</p>
+        </div>
+      </div>
     </header>
   )
 }
+
+
+
 
 export default Header;
--- pages/index.js
+++ pages/index.js
@@ -14,22 +14,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Navbar />
+      <Header />
+
       <main className={styles.main}>
-        <Navbar />
-        <Header />
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        Powered by{' '}
+        <span className={styles.logo}>
+          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+        </span>
       </footer>
     </div>
   )
